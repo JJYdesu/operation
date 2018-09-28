b1b5e1b8b6b84261f41f781602faa92357f00c60
3116005181 <1470064691@qq.com>
2018-09-28 21:59:47 +0800

    _print函数bug修复

--- Operation/src/formula/Test.java
+++ Operation/src/formula/Test.java
@@ -8,10 +8,10 @@ public class Test {
         Formula a, b, c, d, e, f, g;
         ArrayList<Formula> list = new ArrayList<Formula>();
 
-        a = new Formula(Formula.Operators.ADD, 3);
-        b = new Formula(Formula.Operators.ADD, 4, 1, 2);
-        c = new Formula(Formula.Operators.SUB, 8, 6);
-        d = new Formula(Formula.Operators.DIV, 6, 1, 3);
+        a = new Formula(Formula.Operators.ADD, 3); // 3
+        b = new Formula(Formula.Operators.ADD, 4, 1, 2); // 4'1/2
+        c = new Formula(Formula.Operators.SUB, 8, 6); // -8/6
+        d = new Formula(Formula.Operators.DIV, 6, 1, 3); // 除 6'1/3
 
        /* list.add(b);
         list.add(c);
@@ -26,20 +26,32 @@ public class Test {
 //        f.print();
 //        System.out.println(f.symbol+ "*("+ f.integer + "'" + f.numerator+"/"+ f.denominator+")");*/
 
-        ArrayList<Formula> list3 = new ArrayList<Formula>();
-        list3.add(a);
-        list3.add(b);
-        g = new Formula(Formula.Operators.ADD, list3);
-        ArrayList<Formula> list4 = new ArrayList<Formula>();
-        list4.add(c);
-        list4.add(g);
-        list4.add(d);
-        f = new Formula(Formula.Operators.ADD, list4);
-        f.print();
-        System.out.println(f.output());
+//        ArrayList<Formula> list3 = new ArrayList<Formula>();
+//        list3.add(a);
+//        list3.add(b);
+//        g = new Formula(Formula.Operators.ADD, list3);
+//        ArrayList<Formula> list4 = new ArrayList<Formula>();
+//        list4.add(c);
+//        list4.add(g);
+//        list4.add(d);
+//        f = new Formula(Formula.Operators.ADD, list4);
+//        f.print();
+//        System.out.println(f.output());
 //        f.print();*/
 //        String s ;
 //        s = f.symbol+ "*("+ f.integer + "'" + f.numerator+"/"+ f.denominator+")";
 //        System.out.println(s);
+
+        f = new Formula();
+        f.push(a);
+        f.push(b);
+        f.push(c);
+        e = new Formula();
+        e.push(f);
+        e.push(d);
+        e.print();
+        String s = e.output();
+        System.out.println(s);
+
     }
 }
